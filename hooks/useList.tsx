d51dc7cc7a15969cb69eef36f5bd0cf76382f075
Zhao Yang <anhtudo97@gmail.com>
2022-05-17 23:02:10 +0700

    feat(update): config

--- hooks/useList.tsx
+++ hooks/useList.tsx
@@ -1,27 +1,28 @@
 import { collection, DocumentData, onSnapshot } from 'firebase/firestore'
-import { setMaxListeners } from 'process'
 import { useEffect, useState } from 'react'
 import { db } from '../firebase'
 import { Movie } from '../typings'
 
-export default function useList(uid: string | undefined){
-    const []  = useState<DocumentData[] | Movie[]>([])
+function useList(uid: string | undefined) {
+  const [list, setList] = useState<DocumentData[] | Movie[]>([])
 
-    useEffect(()=>{
-        if(!uid) return 
+  useEffect(() => {
+    if (!uid) return
 
-        return onSnapshot(
-            collection(db, 'customers', uid, 'myList'), 
-            (snapshot) => {
-                setMaxListeners(
-                    snapshot.docs.map(doc => ({
-                        id: doc.id,
-                        ...doc.data(),
-                    }))
-                )
-            }
+    return onSnapshot(
+      collection(db, 'customers', uid, 'myList'),
+      (snapshot) => {
+        setList(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
         )
-    }. [db, uid])
+      }
+    )
+  }, [db, uid])
 
-    return list
+  return list
 }
+
+export default useList
